8676bed0055658b323b03b5f4a7e95f57ed89294
Sir_Noodles <smohamedamine@ept.sn>
2024-12-31 17:42:45 +0000

    finished front nettoyer page

---
+++ src/app/components/drag-drop/drag-drop.component.ts
@@ -0,0 +1,78 @@
+import { Component, signal, ViewChild, ElementRef } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import {MatIconModule} from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-drag-drop',
+  templateUrl: './drag-drop.component.html',
+  styleUrl: './drag-drop.component.scss',
+  imports: [MatIconModule, CommonModule],
+
+})
+export class DragDropComponent {
+  audioName = signal('');
+  fileSize = signal(0);
+  uploadProgress = signal(0);
+  audioPreview = signal('');
+  @ViewChild('fileInput') fileInput: ElementRef | undefined;
+  selectedFile: File | null = null;
+  uploadSuccess: boolean = false;
+  uploadError: boolean = false;
+
+  constructor(private snackBar: MatSnackBar) {}
+
+  // Handler for file input change
+  onFileChange(event: any): void {
+    const file = event.target.files[0] as File | null;
+    this.uploadFile(file);
+  }
+
+  // Handler for file drop
+  onFileDrop(event: DragEvent): void {
+    event.preventDefault();
+    const file = event.dataTransfer?.files[0] as File | null;
+    this.uploadFile(file);
+  }
+
+  // Prevent default dragover behavior
+  onDragOver(event: DragEvent): void {
+    event.preventDefault();
+  }
+
+  // Method to handle file upload
+  uploadFile(file: File | null): void {
+    if (file && file.type.startsWith('audio/')) {
+      this.selectedFile = file;
+      this.fileSize.set(Math.round(file.size / 1024)); // Set file size in KB
+
+      const reader = new FileReader();
+      reader.onload = (e) => {
+        this.audioPreview.set(e.target?.result as string); // Set audio preview URL
+      };
+      reader.readAsDataURL(file);
+
+      this.uploadSuccess = true;
+      this.uploadError = false;
+      this.audioName.set(file.name); // Set audio name
+    } else {
+      this.uploadSuccess = false;
+      this.uploadError = true;
+      this.snackBar.open('Seuls les fichiers audios sont autorisés!', 'fermer', {
+        duration: 3000,
+        panelClass: 'error',
+      });
+    }
+  }
+
+  // Method to remove the uploaded audio
+  removeaudio(): void {
+    this.selectedFile = null;
+    this.audioName.set('');
+    this.fileSize.set(0);
+    this.audioPreview.set('');
+    this.uploadSuccess = false;
+    this.uploadError = false;
+    this.uploadProgress.set(0);
+  }
+}
--- src/app/nettoyer/nettoyer.component.ts
+++ src/app/nettoyer/nettoyer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { AudioPlayerComponent } from "../components/audio-player/audio-player.component";
 import { MessageService } from 'primeng/api';
+import { DragDropComponent } from "../components/drag-drop/drag-drop.component";
 @Component({
   selector: 'app-nettoyer',
-  imports: [AudioPlayerComponent],
+  imports: [AudioPlayerComponent, DragDropComponent],
   providers: [MessageService],
   templateUrl: './nettoyer.component.html',
   styleUrl: './nettoyer.component.scss'
