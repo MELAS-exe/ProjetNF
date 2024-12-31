0b62805be78e5c9ac6651396aa0f9dce228de78a
Sir_Noodles <smohamedamine@ept.sn>
2024-12-31 12:09:28 +0000

    added principe

---
+++ src/app/components/audio-player/audio-player.component.ts
@@ -0,0 +1,54 @@
+import { AfterViewInit, Component, ElementRef, input, OnDestroy, signal, ViewChild } from '@angular/core';
+import WaveSurfer from 'wavesurfer.js';
+
+@Component({
+  selector: 'app-audio-player',
+  imports: [],
+  templateUrl: './audio-player.component.html',
+  styleUrl: './audio-player.component.scss'
+})
+export class AudioPlayerComponent implements AfterViewInit, OnDestroy {
+  @ViewChild('playButton', {static: true}) playButton!: ElementRef<HTMLImageElement>;
+  id = input.required<string>();
+  url = input.required<string>();  
+  isPlaying = false;
+  wave: any;
+  
+  ngAfterViewInit(): void {
+    this.wave = WaveSurfer.create({
+        container: '#' + this.id(),
+        height: 30,
+        waveColor: 'black',
+        progressColor: 'rgb(145,202,97)',
+        width: 210,
+        url: this.url(),
+    
+                // Set a bar width
+                barWidth: 3,
+                // Optionally, specify the spacing between bars
+                barGap: 3,
+                // And the bar radius
+                barRadius: 3,
+      });
+  } 
+
+  playPause(): void{
+    if(this.isPlaying){
+      this.playButton.nativeElement.src = 'assets/Play.png';
+      this.wave.pause();
+    }else{
+      this.playButton.nativeElement.src = 'assets/Pause.png';
+      this.wave.play();
+    }
+    this.isPlaying = !this.isPlaying;
+  }
+
+  ngOnDestroy() {
+    this.wave.destroy();
+  }
+
+  constructor() {
+    
+  }
+  
+}
--- src/app/nettoyer/nettoyer.component.ts
+++ src/app/nettoyer/nettoyer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
-
+import { AudioPlayerComponent } from "../components/audio-player/audio-player.component";
+import { MessageService } from 'primeng/api';
 @Component({
   selector: 'app-nettoyer',
-  imports: [],
+  imports: [AudioPlayerComponent],
+  providers: [MessageService],
   templateUrl: './nettoyer.component.html',
   styleUrl: './nettoyer.component.scss'
 })
